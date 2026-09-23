96db09f9e5d8d09d98342eeb625b9773802647ea
mvega1212 <ysd4v8sy6p@privaterelay.appleid.com>
2026-09-23 12:02:23 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='virus-drop-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+const CACHE='virus-drop-v4';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(r.ok){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));}return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
