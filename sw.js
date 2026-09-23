fc1f48c5b30c1fc1c800d88e09aa22f774bcb722
mvega1212 <ysd4v8sy6p@privaterelay.appleid.com>
2026-09-23 10:44:57 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='virus-drop-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-512.png']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
